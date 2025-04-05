1b5ce2fcc31676752b2438df2f05c309b657d73c
イ·スンミン <is3686994@gmail.com>
2025-04-05 19:45:16 +0900

    pygame-2

--- pygame1.py
+++ pygame1.py
@@ -1,21 +1,43 @@
-import pygame
+# pygame 모듈 가져오기 
+import pygame 
 
-# pygame을 사용하기 위한 초기화 
+#pygame 사용을 위한 초기화 
 pygame.init() 
 
-# 창 크기 설정 
-WINDOW_WIDTH = 600 
+# 창 크기 설정
+WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 300
 
-# 창에 대한 설정 
-display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption('Hello World!')
+# RGE 색상 기준으로 사용할 색깔 정의 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE =(0, 0, 255)
 
-# 게임이 동작하는 동안 이벤트 
-running = True 
+# 화면 설정 
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+# 백 그라운드 색깔 설정 
+display_surface.fill(BLUE)
+
+# line 함수를 이용하여 라인 그리기 
+pygame.draw.line(display_surface, RED, (100, 100), (200, 200), 3)
+
+# circule() 함수를 이용하여 하얀색으로 동그라미 그리기
+pygame.draw.circle(display_surface, WHITE, (WINDOW_WIDTH//2, WINDOW_HEIGHT//2), 50, 3)
+
+# 이름 설정 
+pygame.display.set_caption('이승민ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ')
+
+# 화면 계속 유지하고 event type이 QUIT일 경우 종료
+running = True
 while running :
     for event in pygame.event.get() :
         if event.type == pygame.QUIT :
             running = False
+    #디스플레이 업데이트 
+    pygame.display.update() 
 
+# 화면 종료 
 pygame.quit()
