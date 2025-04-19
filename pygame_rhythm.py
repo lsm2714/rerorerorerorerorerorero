6f0d2a0857faf54b61f3a381f67277cb0e2dd00d
イ·スンミン <is3686994@gmail.com>
2025-04-19 17:42:28 +0900

    파이리즈무 복습

--- pygame_rhythm.py
+++ pygame_rhythm.py
@@ -1,130 +1,125 @@
 # pygame 모듈 불러오기 
-import pygame 
+import pygame
 
-# 원활한 실행을 위해 init()로 pygame 초기화 
+# 원활한 실행을 위해 init()으로 pygame 초기화
 pygame.init() 
 
-# 화면 크기 설정
+# 화면 크기 설정 
 WINDOW_WIDTH = 600
-WINDOW_HEIGHT = 500 
-
-# 콤보 텍스트 설정 
-COMBO = 0 
-
-# 판정 시간, 판정 텍스트 미리 설정 
-judgement_time = 0
-judgement_text = ''
+WINDOW_HEIGHT = 500
 
 # FPS 설정 
 clock = pygame.time.Clock() 
 
-# 음악 넣기 
-pygame.mixer.music.load('INSOMNIA.mp3')
+# 음악 설정 
+pygame.mixer.music.load('INSOMNIA.mp3') 
 pygame.mixer.music.set_volume(0.4) 
-pygame.mixer.music.play()
+pygame.mixer.music.play() 
 
-# 화면 크기 적용, 이름 설정하기 
+# 콤보 설정 
+COMBO = 0 
+
+# 판정 텍스트, 판정 시간 설정 
+judgement_time = 0
+judgement_text = ''
+
+# 판정, 콤보 텍스트 폰트 설정 
+impact_font = pygame.font.SysFont('impact', 20)
+
+# 화면 크기 적용, 이름 설정 
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('파이리즈무 : INSOMNIA')
 
-# 판정 폰트 설정 
-impact_font = pygame.font.SysFont('impact', 20)
-
-# 노트 리스트 설정 : 시간, x, y 좌표
+# 노트 리스트 설정 
 notes = [
     {'time' : 1000, 'x' : 75, 'y' : -50},
-    {'time' : 2000, 'x' : 225, 'y' : -50},
-    {'time' : 3000, 'x' : 375, 'y' : -50},
+    {'time' : 3000, 'x' : 225, 'y' : -50},
+    {'time' : 2000, 'x' : 375, 'y' : -50},
     {'time' : 4000, 'x' : 525, 'y' : -50}
 ]
 
-# 현재 떨어지는 노트를 따로 분리할 리스트 선언 
-falling_notes = []
+# 떨어질 때의 노트 리스트 설정 
+falling_notes = [] 
 
-# ASDF 키 리스트 설정 
-key_maps = {
-    pygame.K_a : 75,
-    pygame.K_s : 225,
-    pygame.K_d : 375,
+# 키 리스트 설정 
+key_maps = { 
+    pygame.K_a : 75, 
+    pygame.K_s : 225, 
+    pygame.K_d : 375, 
     pygame.K_f : 525
 }
 
-# 화면이 계속 실행될 수 있게 while 반복문으로 True 조건 실행하기
-running = True 
+# 화면이 계속 출력될 수 있도록 while 반복문에 True 조건 설정 
+running = True
 while running :
-    # 현재 시간 저장 
-    current_time = pygame.time.get_ticks() 
+    current_time = pygame.time.get_ticks() # 현재 흘러가는 시간을 변수에 실시간으로 저장 
     
     for event in pygame.event.get() :
         if event.type == pygame.QUIT :
             running = False 
             
-    # for 반복문 활용하여 특정 시간이 되면 떨어지는 노트 따로 분리, 원래 리스트의 노트를 삭제 
+    # 화면 fill() 로 칠해서 노트 등이 보이게 하기 
+    display_surface.fill((0, 0, 0))
+    
+    # 특정 시간이 되면 떨어지기 전의 노트를 삭제하고 falling_notes리스트에 따로 저장하기
     for note in notes[:] :
         if current_time >= note['time'] :
             falling_notes.append(note) 
             notes.remove(note) 
-    
-    # 화면 검은색으로 덮어서 이미지가 움직일 때 겹쳐 보이지 않게 하기 
-    display_surface.fill((0, 0, 0))
-    
-    # 떨어지는 노트 그리기, 이동 속도 설정 (시간 기반) 
-    NOTE_SPEED = 0.7 # 속도 설정 (곱할 것이기 때문에 1 이하가 적당)
+            
+    # falling_notes 리스트에 있는 노트 아래로 떨어뜨리기 (시간 기반)
+    NOTE_SPEED = 0.5 
     for note in falling_notes :
-        # 노트 시작 변수는 현재 시간 - 특정 시간 을 해서 값이 점점 커지게 하기 
-        time_since_note_start = current_time - note['time'] 
-        # 노트가 떨어지는 속도 설정 
+        time_since_note_start = current_time - note['time'] # 현재 시간 - 특정 시간을 설정하여 0부터 증가하는변수 만들기 
         note['y'] = -50 + NOTE_SPEED * time_since_note_start 
-        # 사각형의 노트 그리기 
-        pygame.draw.rect(display_surface, (255, 0, 255), (note['x'], note['y'], 50, 10))
+        pygame.draw.rect(display_surface, (255, 0, 255), (note['x'], note['y'], 50, 14))
         
-        # 키 설정
-        keys = pygame.key.get_pressed() # 누른 키를 실시간으로 keys 변수에 전달 
-        
-        # 노트 판정, 판정 텍스트 출력 
+        # 키 설정 
+        keys = pygame.key.get_pressed() 
+        # 노트 판정, 콤보 수 증가 설정 
         for key, x in key_maps.items() :
-            if keys[key] : # 여기서 누른 키의 값에 따라 그에 맞는 x값을 가져온다. 
-                for note in falling_notes[:] :
-                    if abs(note['x'] - x) < 5 and abs(note['y'] - 400) < 30 : 
-                        offset = note['y'] - 400 
-                        abs_offset = abs(offset) # 절댓값으로 바꿔주는 이유 : 
-                        # 이게 바꾸기 전( 그냥 offset 값으로 판정 설정 )에서는 371의 위치에서 클릭해도 PERFECT가 떴던 건 
-                        # 371 - 400 = -29 로 첫 번째 if 조건식 ( < 10 )과 일치하니까 PERFECT가 출력됬던거고 이제 이걸 절댓값으로 바꾸니까 
-                        # -가 사라져서 맨 위에서 클릭하면 29가 되서 MISS 출력
-                        # 옵셋 값에 따라 판정 텍스트, 콤보 설정 
+            if keys[key] :
+                for note in falling_notes[:] : 
+                    if abs(note['x'] - x) < 5 and abs(note['y'] - 400) < 35 :
+                        # abs()로 +로 바꿔주지 않으면 -34도 아래 조건에 의해 PERFECT가 출력되기 때문에 abs() 절댓값 함수로 값을 바꿔줘야 한다. 
+                        abs_offset = abs(note['y'] - 400) 
+                        # 판정 텍스트, 콤보 설정 
                         if abs_offset < 10 :
                             COMBO += 1 
                             judgement_text = 'PERFECT'
-                            result_text = impact_font.render(judgement_text, True, (255, 0, 255), None)
+                            result_surface = impact_font.render(judgement_text, True, (255, 0, 255), None)
                         elif abs_offset < 20 :
                             COMBO += 1 
-                            judgement_text = 'GREAT'
-                            result_text = impact_font.render(judgement_text, True, (255, 240, 71), None)
+                            judgement_text = 'GOOD'
+                            result_surface = impact_font.render(judgement_text, True, (214, 214, 0), None)
                         else :
                             COMBO = 0
-                            judgement_text = 'MISS'
-                            result_text = impact_font.render(judgement_text, True, (214, 214, 214), None)
-                        # 위의 조건식이 True이면 ASDF 중 하나 누른 후 note 삭제 
-                        judgement_time = current_time # 노트 삭제와 동시에 노트가 사라지니까 위의 조건식이 초기화가 되어서 딱 그 순간의 값이 저장됨 
+                            judgement_text = 'BAD'
+                            result_surface = impact_font.render(judgement_text, True, (0, 214, 104), None)
+                        # 위 조건이 참일 시 노트 삭제 
                         falling_notes.remove(note)
-                        break # 오류 방지 
+                        judgement_time = current_time
+                        break 
+                    # 키를 클릭하고 노트를 삭제하고 break를 걸면 위의 조건식이 False가 되므로 
+                    # 딱 그 시간의 값이 judgement_time에 저장된다. 
+                    # break를 걸면 다시 키를 누르기 전까지 위 조건식이 거짓이 되므로 두 개의 노트가 동시에 판정되는 오류를 막을 수 있다. 
         
-    # 판정선 그리기 
-    pygame.draw.line(display_surface, (214, 214, 214), (0, 400), (WINDOW_WIDTH, 400), 4)
+    # 판정선 그리기
+    pygame.draw.line(display_surface, (214, 214, 214), (0, 400), (WINDOW_WIDTH, 400), 4)  
     
-    # 판정 텍스트 출력 (1초 동안) 
-    if current_time - judgement_time < 1000 and judgement_text :
-        display_surface.blit(result_text, (300, 450))
-        
-    # 콤보 수 출력 
+    # 콤보 텍스트 설정
     combo_text = impact_font.render(f'COMBO : {COMBO}', True, (255, 0, 255), None)
     display_surface.blit(combo_text, (10, 10))
     
-    # 화면 계속 업데이트
+    # 판정 텍스트 출력 (1초 동안) 
+    if current_time - judgement_time < 1000 and judgement_text :
+        display_surface.blit(result_surface, (250, 450))
+              
+    # 화면 업데이트
     pygame.display.update() 
-        
+    
     # FPS 설정 
-    clock.tick(60)
+    clock.tick(60) 
 
-# 화면의 원활한 종료를 위해 quit() 설정 
+# 원활한 종료를 위해 quit() 설정 
 pygame.quit() 
