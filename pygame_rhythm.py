5ccfc5d4c6ff6df05fc57ee789c894e9770bf8da
イ·スンミン <is3686994@gmail.com>
2025-04-23 12:33:23 +0900

    파이리즈무 MISS 추가, 노트 추가 생성 쉽게

--- pygame_rhythm.py
+++ pygame_rhythm.py
@@ -1,6 +1,9 @@
 # pygame 모듈 불러오기 
 import pygame
 
+# csv 모듈 불러오기
+import csv 
+
 # 원활한 실행을 위해 init() 설정 
 pygame.init() 
 
@@ -12,15 +15,15 @@ WINDOW_HEIGHT = 500
 clock = pygame.time.Clock()
 
 # 음악 설정 
-pygame.mixer.music.load('INSOMNIA.mp3')
-pygame.mixer.music.set_volume(0.4)
+pygame.mixer.music.load('Snow_Drop.mp3')
+pygame.mixer.music.set_volume(0.4) 
 pygame.mixer.music.play() 
 
 # 콤보 설정 
 COMBO = 0 
 
 # 판정 텍스트, 판정 시간 변수 설정 
-judgement_text = ''
+judgement_text = ''   
 judgement_time = 0
 
 # 텍스트 폰트 설정 
@@ -28,15 +31,21 @@ impact_font = pygame.font.SysFont('impact', 20)
 
 # 화면 크기 적용, 이름 설정 
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption('파이리즈무 : INSOMNIA') 
+pygame.display.set_caption('파이리즈무 : Snow_Drop') 
 
-# 노트 리스트 설정 : 시간, x, y 좌표
-notes = [
-    {'time' : 1000, 'x' : 75, 'y' : -50},
-    {'time' : 3000, 'x' : 225, 'y' : -50},
-    {'time' : 2000, 'x' : 375, 'y' : -50},
-    {'time' : 4000, 'x' : 525, 'y' : -50}
-]
+# 노트 리스트 초기화
+notes = [] 
+# open()으로 파일 불러오기, newline='' 으로 빈줄 오류 방지, as로 note_file의 형태로 저장 
+with open('notes.csv', newline='') as note_file : # with를 쓰는 이유는 아래에서 모든 작업을 끝낸 후 자동으로 close() 닫아줘야 오류를 막을 수 있기 때문 
+    # DictReader() 로 note_file를 딕셔너리 값으로 변환 후 저장 
+    reader = csv.DictReader(note_file) 
+    # 딕셔너리 값을 노트 리스트에 append()로 옮기기, 그 안에서도 딕셔너리 값 설정 
+    for note_data in reader :
+        notes.append({
+            'time' : int(note_data['time']),
+            'x' : int(note_data['x']),
+            'y' : int(note_data['y'])
+        })
 
 # 떨어질 때의 리스트 설정 
 falling_notes = []
@@ -73,7 +82,7 @@ while running :
         time_since_note_start = current_time - note['time'] 
         note['y'] = -50 + NOTE_SPEED * time_since_note_start 
         pygame.draw.rect(display_surface, (255, 0, 255), (note['x'], note['y'], 50, 10))
-        
+
         # 키 입력에 따라 판정 설정 (노트가 떨어질 때 실행되야 하므로 움직이는 for문 아래서 실행)
         keys = pygame.key.get_pressed() # 현재 누르는 키 실시간으로 확인
         for key, x in key_maps.items() : # 딕셔너리 값 하나하나 저장 
@@ -101,13 +110,24 @@ while running :
                         falling_notes.remove(note)
                         judgement_time = current_time # 누르자마자 노트가 사라지므로 위 조건식이 False가 되어 딱 그 시간이 저장됨 
                         break 
+                    
+        # 놓친 노트 MISS 판정
+        for note in falling_notes[:] :
+            if note['y'] > 460 : 
+                COMBO = 0
+                judgement_text = 'MISS' 
+                result_surface = impact_font.render(judgement_text, True, (214, 214, 214), None)
+                # 460을 초과한 노트를 삭제하여 위 조건식을 False로 만들기 ( False로 만들어 줘야 다음 노트 판정 시 올바른 값이 출력된다. )
+                falling_notes.remove(note) 
+                # MISS 판정도 1초 동안 출력하기 위해 판정 시간 설정 
+                judgement_time = current_time
     
     # 콤보 텍스트 출력
     COMBO_TEXT = impact_font.render(f'COMBO : {COMBO}', True, (255, 0, 255), None)
     display_surface.blit(COMBO_TEXT, (10, 10)) 
     
     # 판정 텍스트 출력 (1초 동안) 
-    # 현재 시간 - 누를 때의 시간 and 판정 텍스트가 모두 True일 경우 실행
+    # 현재 시간 - 누를 때의 시간이 1000보다 작고 and 판정 텍스트가 True일 경우 실행
     if current_time - judgement_time < 1000 and judgement_text :
         display_surface.blit(result_surface, (250, 420))
     
