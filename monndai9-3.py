1a5f681dc39a0ba33919b05c47689fe783c71872
イ·スンミン <is3686994@gmail.com>
2025-06-06 14:45:19 +0900

    바닐라

--- monndai9-3.py
+++ monndai9-3.py
@@ -1,53 +1,37 @@
-# 추가값 넣을 리스트 만들기 
-list_value = []
+# 문자열 검색 및 위치 찾기 (바닐라 버전) 
+text = 'bar bar pos kin kin kin sol bar foo bar pos kin foo' 
 
-# 반복 설정 
-while True :
-    num = int(input('''
-작업을 선택하세요 :
-1: 추가 (Create)
-2: 조회 (Read)
-3: 수정 (Update)
-4: 삭제 (Delete)
-5: 종료 (Exit)
-입력 : '''))
+# 문자열 넣을 리스트 추가 
+word_list = []  
+
+# 리스트에 띄어쓰기를 기준으로 문자열 집어넣기 
+word = ''
+for w in text :
+    if w == ' ' :
+        word_list.append(word)
+        word = '' 
+        continue
+    word += w 
+# 마지막 문자열도 추가 
+word_list.append(word) 
+
+# 찾을 문자열 입력 
+find_word = input('찾을 문자열을 입력하세요 : ') 
+
+# 만약 find_word가 리스트 안에 있을 경우 count 증가로 등장 횟수 출력, 인덱스의 위치 출력 
+if find_word in word_list :
+    count1 = 0
+    count2 = 0
+    index_list = []
+    for find1 in word_list :
+        if find1 == find_word :
+            count1 += 1
+    print(f'"{find_word}는 총 {count1}번 등장합니다."')
+    for find2 in word_list :
+        if find2 == find_word :
+            index_list.append(count2)
+        count2 += 1 
+    print(f'위치 (인덱스) : {index_list}')
+else :
+    print('입력하신 단어는 존재하지 않습니다.')
     
-    # 인덱스 리스트 설정 
-    list_index = []
-    for i, ch in enumerate(list_value) :
-        list_index.append(i)
-
-    # 종료 설정 
-    if num == 5 :
-        print('프로그램을 종료합니다.')
-        break
-    # 추가 설정 
-    elif num == 1 :
-        input_value = input('추가할 값을 입력하세요 : ')
-        list_value.append(input_value)
-        print('추가 완료.')
-    # 조회 설정 
-    elif num == 2 :
-        print('\n[현재 리스트 내용]')
-        for i, ch in enumerate(list_value) :
-            print(f'{i} : {ch}')
-    # 수정 설정 
-    elif num == 3 :
-        input_value = int(input('수정할 인덱스를 입력하세요 : '))
-        if input_value in list_index :
-            new = input('새로운 값을 입력하세요 : ')
-            list_value[input_value] = new 
-            print('수정 완료.')
-        else : 
-            print('유효하지 않은 인덱스입니다.')
-    # 삭제 설정 
-    elif num == 4 :
-        input_value = int(input('삭제할 인덱스를 입력하세요 : '))
-        if input_value in list_index :
-            del list_value[input_value]
-            print('삭제 완료.')
-        else :
-            print('유효하지 않은 인덱스입니다.')
-    # 잘못된 입력 설정 
-    else :
-        print('잘못된 입력입니다. 올바른 번호를 입력하세요.')
