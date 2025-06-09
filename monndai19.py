c974a531f6902aa20f2d233419977a8304f7b8ab
イ·スンミン <is3686994@gmail.com>
2025-06-09 15:51:08 +0900

    점수 분류 및 평균 계산

--- monndai19.py
+++ monndai19.py
@@ -1,21 +1,19 @@
-# 점수 분류 및 평균 계산 
-# 학생들의 점수 리스트 
+# 학생들의 점수 리스트
 scores = [92, 85, 34, 76, 58, 90, 61, 70, 45, 99, 82, 67, 50, 77, 89] 
 
-# 점수에 따라 등급 분류 (딕셔너리)
-list_value_Dict = {
-    'A' : [i for i in scores if i >= 90],
-    'B' : [i for i in scores if 90 > i >= 80],
-    'C' : [i for i in scores if 80 > i >= 70], 
-    'D' : [i for i in scores if 70 > i >= 60], 
-    'F' : [i for i in scores if i < 60] 
-}
+# 등급 딕셔너리 만들기 
+dictionary = {'A' : [val for val in scores if val >= 90],
+         'B' : [val for val in scores if 90 > val >= 80],
+         'C' : [val for val in scores if 80 > val >= 70],
+         'D' : [val for val in scores if 70 > val >= 60],
+         'F' : [val for val in scores if 60 > val] }
 
-# 등급 출력, 평균 점수도 
-print('등급 분포 및 평균 점수 : ')
-# 딕셔너리 값을 하나하나 저장하기
-for ABCDF, list_value in list_value_Dict.items() :
-    print(f'{ABCDF}등급 : 평균 점수 = {(sum(list_value) / len(list_value)):.2f}')
-    for i in list_value : 
+# 등급 분포 및 평균 점수 출력 
+print('등급 분포 및 평균 점수 :') 
+for ABCDF, numbers_list in dictionary.items() :
+    print(f'{ABCDF}등급 : 평균 점수 = {(sum(numbers_list) / len(numbers_list)):.2f}')
+    count = 0 
+    for _ in numbers_list : 
         print('*', end='')
-    print(f' {len(list_value)}명') 
+        count += 1
+    print(f' ({count}명)')
