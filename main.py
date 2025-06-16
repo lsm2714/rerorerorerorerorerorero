f2d1d64d272d71ce54a80edc04dd54bd101928de
イ·スンミン <is3686994@gmail.com>
2025-06-16 16:18:55 +0900

    합계 계산 함수 연습

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
-def generate_profile(name, age, gender='미정', *interests, **metadata) :
-    print(f'''[고객 프로필]
-이름 : {name}
-나이 : {age}
-성별 : {gender}''')
-    if interests :
-        print('관심사: ', end='')
-        for value in interests[:-1] :
-            print(value, end=', ')
-        print(interests[-1]) 
-    if metadata :
-        print('추가 정보: ', end='')
-        list_kv = []
-        for k, v in metadata.items() :
-            kv = f'{k}={v}'
-            list_kv.append(kv)
-        print(', '.join(list_kv))
+def add_numbers(*args, **kwargs) :
+    if kwargs :
+        for key, value in kwargs.items() :
+            if key == 'abs' and value == True :
+                list_abs = []
+                for num in args :
+                    list_abs.append(abs(num))
+            elif key == 'only_even' and value == True :
+                list_num = []
+                for num in list_abs :
+                    if num % 2 == 0 :
+                        list_num.append(num)
+                print(f'합계는 {sum(list_num)}입니다.')
+            elif key == 'unique' and value == True :
+                list_num2 = []
+                for num in args :
+                    if list_num2 and num == list_num2[-1] :
+                        continue
+                    list_num2.append(num) 
+                print(f'합계는 {sum(list_num2)}입니다.')
+            else :
+                print(None)
+                        
+    else :
+        print(f'합계는 {sum(args)}입니다.')
 
-generate_profile('홍길동', 30)
+add_numbers(1, -2, 2, -3)
 
-generate_profile('지민', 26, '여성', *['여행', '사진', '독서'], job='디자이너', country='한국')
+add_numbers(1, -2, 2, -3, abs=True, only_even=True)
+
+add_numbers(1, 2, 2, 3, 3, 4, unique=True)
+
+add_numbers(1, 2, 3, round=True)
